c55fb0829c5277b1cb8b0f0c7af6ef643cddef82
Tommy_Tan <sage.ts920126@gmail.com>
2017-12-07 16:52:26 +0800

    add Admin::BaseController and move authenticate_admin here

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,5 +1,5 @@
-class Admin::CategoriesController < ApplicationController
-  
+class Admin::CategoriesController < Admin::BaseController
+
   before_action :authenticate_admin
   before_action :set_category, only: [ :edit, :update, :destroy]
 
