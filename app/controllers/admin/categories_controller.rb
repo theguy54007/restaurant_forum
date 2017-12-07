d9efafda5a9c0cd6b82c5c449d0d5dfb81d23a61
Tommy_Tan <sage.ts920126@gmail.com>
2017-12-07 16:15:30 +0800

    user can delete category with link on index page

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -35,6 +35,18 @@ class Admin::CategoriesController < ApplicationController
       render :index
     end
   end
+
+  def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+    flash[:alert] = "category was successfully deleted"
+    redirect_to admin_categories_path
+  end
+
+
+
+
+
   private
 
 
