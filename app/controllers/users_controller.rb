f7907f3395ef0ddb529231ef24cbd0598467c172
Tommy_Tan <sage.ts920126@gmail.com>
2017-12-29 16:40:08 +0800

    user can upload avatar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :intro)
+    params.require(:user).permit(:name, :intro, :avatar)
   end
 
   def set_user
