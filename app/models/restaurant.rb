305604509da0123683b07404cf54b8fb1417410b
Tommy_Tan <sage.ts920126@gmail.com>
2018-01-04 20:20:51 +0800

    create favorite model & setup relation among user & restaurant & favorite

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -4,6 +4,8 @@ class Restaurant < ApplicationRecord
   belongs_to :category, optional: true
   has_many :comments, dependent: :destroy
 
+  has_many :favorites, dependent: :destroy
+  has_many :favorited_users, through: :favorites, source: :user
 
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171229034517) do
+ActiveRecord::Schema.define(version: 20180104120850) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -24,6 +24,13 @@ ActiveRecord::Schema.define(version: 20171229034517) do
     t.text "content"
     t.integer "restaurant_id"
     t.integer "user_id"
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "restaurant_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "restaurants", force: :cascade do |t|
