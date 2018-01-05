449b916544580469c2cbfefaa44bac2facd4754a
Tommy_Tan <sage.ts920126@gmail.com>
2018-01-05 10:10:25 +0800

    count favorites size in favorite/ unfavorite action

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -22,6 +22,7 @@ class RestaurantsController < ApplicationController
   def favorite
     @restaurant = Restaurant.find(params[:id])
     @restaurant.favorites.create!(user: current_user)
+    @restaurant.count_favorites
     redirect_back(fallback_location: root_path)
 
   end
@@ -30,6 +31,7 @@ class RestaurantsController < ApplicationController
     @restaurant = Restaurant.find(params[:id])
     favorite = Favorite.where(restaurant: @restaurant, user: current_user)
     favorite.destroy_all
+    @restaurant.count_favorites
     redirect_back(fallback_location: root_path)
   end
 
@@ -45,5 +47,8 @@ class RestaurantsController < ApplicationController
     like.destroy_all
     redirect_back(fallback_location: root_path)
   end
+
+
+
 
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -18,4 +18,11 @@ class Restaurant < ApplicationRecord
   def is_liked?(user)
     self.liked_users.include?(user)
   end
+
+  def count_favorites
+    self.favorites_count = self.favorites.size
+    self.save
+  end
+
+
 end
