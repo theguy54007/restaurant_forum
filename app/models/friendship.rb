dfe0bd785a0c2b918c40490e4ac65282bbb45732
Tommy_Tan <sage.ts920126@gmail.com>
2018-01-05 22:03:19 +0800

    setup friend and unfriend button on user index

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,5 +1,5 @@
 class Friendship < ApplicationRecord
-  validate :friend_id, uniqueness: {scope: :user_id} 
+  validates :friend_id, uniqueness: {scope: :user_id} 
   belongs_to :user
   belongs_to :friends, class_name: "User"
 
